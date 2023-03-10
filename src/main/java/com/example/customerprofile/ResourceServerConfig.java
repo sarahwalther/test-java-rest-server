a172daae45e15353368cc2b12d48c1c6b388dc22
Dmitriy Dubson <ddubson@vmware.com>
2023-03-10 14:38:28 -0500

    Add resource server config

---
+++ src/main/java/com/example/customerprofile/ResourceServerConfig.java
@@ -0,0 +1,46 @@
+package com.example.customerprofile;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class ResourceServerConfig {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	private String issuerUri;
+
+	// @formatter:off
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests(authorizeRequests -> {
+					authorizeRequests.antMatchers(HttpMethod.GET, "/").hasAuthority("SCOPE_message.read");
+					authorizeRequests.antMatchers(HttpMethod.GET, "/customer-profiles/**").hasAuthority("SCOPE_message.read");
+					authorizeRequests.antMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_message.write");
+					authorizeRequests.antMatchers(HttpMethod.POST, "**").hasAuthority("SCOPE_message.write");
+					authorizeRequests.antMatchers(HttpMethod.PATCH, "**").hasAuthority("SCOPE_message.write");
+					authorizeRequests.antMatchers(HttpMethod.DELETE, "**").hasAuthority("SCOPE_message.write");
+					authorizeRequests.anyRequest().denyAll();//?
+				})
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+				.build();
+	}
+	// @formatter:on
+
+	@EventListener
+	public void handleContextRefresh(ContextRefreshedEvent event) {
+		logger.info("Authorization server issuer URI: " + issuerUri);
+	}
+}
