32f217dd32d2845dde8039b32fb8edb01dea2c95
Sarah Walther <sawalther@vmware.com>
2023-03-21 17:36:35 -0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+
+plugins {
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("java")
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+val springdocVersion = "2.0.2"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.testcontainers:postgresql:1.17.6")
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:${springdocVersion}")
+
+    // Infrastructure
+    implementation("org.flywaydb:flyway-core")
+
+    implementation("com.h2database:h2")
+    // ResourceServerStart
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    // ResourceServerEnd
+
+
+    // Test
+    testImplementation ("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
